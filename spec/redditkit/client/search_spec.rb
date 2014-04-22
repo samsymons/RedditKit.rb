f9c432942701413e59afb6d7882c6feaae9caca3
Hasnain Bukhari <strider195@googlemail.com>
2014-04-22 22:41:56 +0100

    Add test for searching link

--- spec/redditkit/client/search_spec.rb
+++ spec/redditkit/client/search_spec.rb
@@ -20,6 +20,11 @@ describe RedditKit::Client::Search, :vcr do
       results = RedditKit.search 'ruby', :subreddit => 'ruby', :restrict_to_subreddit => true, :limit => 3
       expect(results.length).to eq 3
     end
+
+    it "returns search results from the 'seen it' page" do
+      results = RedditKit.search 'http://i.imgur.com/ypLqggl.png'
+      expect(results).to_not be_empty
+    end
   end
 
 end
