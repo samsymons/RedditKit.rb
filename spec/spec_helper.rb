1df754e3aa5a6ac811b547b4a38a1862c096d4d0
Sam Symons <sam@samsymons.com>
2014-08-24 13:26:55 -0700

    Remove a redundant RSpec configuration line.
    `treat_symbols_as_metadata_keys_with_true_values` is now true by default. Nice!

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,10 +12,6 @@ require 'rspec'
 require 'webmock/rspec'
 require 'vcr'
 
-RSpec.configure do |c|
-  c.treat_symbols_as_metadata_keys_with_true_values = true
-end
-
 VCR.configure do |c|
   c.configure_rspec_metadata!
 
