f4c05c6740840ca612a997c58efdf743d13d0a4e
Nicolas Andreas Grunbaum <ngrunbaum+github@nostrum.com>
2014-10-22 18:34:57 -0700

    Adding user agent name and version to client constructor.
    User Agent string is now easy to generate correctly
    
    Client ctor has new optional parameters for constructing the user agent string.
    
    The user agent string can still be set manually.
    
    Added tests relavent tests.

--- spec/redditkit/client_spec.rb
+++ spec/redditkit/client_spec.rb
@@ -4,7 +4,7 @@ describe RedditKit::Client, :vcr do
 
   it "can retrieve default options" do
     expect(RedditKit.api_endpoint).to eq 'http://www.reddit.com/'
-    expect(RedditKit.user_agent).to eq "RedditKit.rb #{RedditKit::Version}"
+    expect(RedditKit.user_agent).to eq "RedditKit.rb/#{RedditKit::Version}"
   end
 
   it "can configure options" do
@@ -12,6 +12,26 @@ describe RedditKit::Client, :vcr do
     client.api_endpoint = "http://github.com/"
 
     expect(client.api_endpoint).to eq "http://github.com/"
+  end
+
+  it "can configure an agent string" do
+    client = RedditKit::Client.new nil,nil,"TestAgentBot","86"
+
+    expect(client.user_agent).to eq "TestAgentBot/86"
+
+
+    client.user_agent_name = "OtherTestAgentBot"
+    client.user_agent_version = 99
+    expect(client.user_agent).to eq "OtherTestAgentBot/99"
+
+    client.user_agent = "ThirdTestAgentBot/7.00 (+http://www.reddit.com/user/user/samsymons)"
+    expect(client.user_agent).to eq "ThirdTestAgentBot/7.00 (+http://www.reddit.com/user/user/samsymons)"
+
+    client.user_agent = nil
+    expect(client.user_agent).to eq "OtherTestAgentBot/99"
+
+    client = RedditKit::Client.new "samsymons",nil,"FourthTestAgentBot","86"
+    expect(client.user_agent).to eq "FourthTestAgentBot/86 (+http://www.reddit.com/user/samsymons)"
   end
 
   it "should have a modhash and cookie after signing in" do
