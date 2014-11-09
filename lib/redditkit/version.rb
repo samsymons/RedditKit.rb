210707419daaffd2401c86e25317e0c5d28ecf9f
Sam Symons <sam@samsymons.com>
2014-11-08 16:38:29 -0800

    Prepare for 1.1.0.

--- lib/redditkit/version.rb
+++ lib/redditkit/version.rb
@@ -3,17 +3,13 @@ module RedditKit
   # A class for managing RedditKit's version number.
   class Version
     MAJOR = 1
-    MINOR = 0
-    PATCH = 1
+    MINOR = 1
+    PATCH = 0
 
     class << self
-
-      # Return RedditKit.rb's version number as a string.
       def to_s
         [MAJOR, MINOR, PATCH].join('.')
       end
-
     end
-
   end
 end
